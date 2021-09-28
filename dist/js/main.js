03200413e715c66cc4968f51e2f00748983243e2
Anna <anna.b.scislowska@gmail.com>
2021-09-28 10:29:47 +0200

    podstawowa funkcjonalność i podstrony

---
+++ dist/js/main.js
@@ -0,0 +1,31 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuBurger = document.querySelector('.menu-btn_burger');
+const nav = document.querySelector('nav');
+const menuNav = document.querySelector('.menu-nav');
+const menuItems = document.querySelectorAll('.menu-nav_item');
+const title = document.querySelector('.title');
+const body = document.querySelector('body');
+let showMenu = false;
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBurger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        menuItems.forEach(item => item.classList.add('open'));
+        showMenu = true;
+    } else {
+        menuBurger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        menuItems.forEach(item => item.classList.remove('open'))
+        showMenu = false;
+    }
+} 
+function showTitle() {
+    console.log("bam!");
+    title.classList.add('title_loaded');
+}
+
+menuBtn.addEventListener('click', toggleMenu);
+window.addEventListener('load', showTitle);
